0c317ff1a556b20838f7d9300dbf69e7e1fba9a5
Max Meinhold <mxmeinhold@gmail.com>
2018-10-31 17:26:13 -0400

    Makes colour programmer friendly (and readable)

--- prompt/colour.sh
+++ prompt/colour.sh
@@ -1,43 +1,40 @@
 # foreground
-colour_black='\[\033[0;30m\]'
-colour_red='\[\033[0;31m\]'
-colour_green='\[\033[0;32m\]'
-colour_yellow='\[\033[0;33m\]'
-colour_blue='\[\033[0;34m\]'
-colour_purple='\[\033[0;35m\]'
-colour_cyan='\[\033[0;36m\]'
-colour_white='\[\033[0;37m\]'
-# bright
-colour_bright_black='\[\033[0;90m\]'
-colour_bright_red='\[\033[0;91m\]'
-colour_bright_green='\[\033[0;92m\]'
-colour_bright_yellow='\[\033[0;93m\]'
-colour_bright_blue='\[\033[0;94m\]'
-colour_bright_purple='\[\033[0;95m\]'
-colour_bright_cyan='\[\033[0;96m\]'
-colour_bright_white='\[\033[0;97m\]'
-
-
+declare -A fg=(
+    ["black"]='\[\033[0;30m\]'
+    ["red"]='\[\033[0;31m\]'
+    ["green"]='\[\033[0;32m\]'
+    ["yellow"]='\[\033[0;33m\]'
+    ["blue"]='\[\033[0;34m\]'
+    ["purple"]='\[\033[0;35m\]'
+    ["cyan"]='\[\033[0;36m\]'
+    ["white"]='\[\033[0;37m\]'
+    ["bright_black"]='\[\033[0;90m\]'
+    ["bright_red"]='\[\033[0;91m\]'
+    ["bright_green"]='\[\033[0;92m\]'
+    ["bright_yellow"]='\[\033[0;93m\]'
+    ["bright_blue"]='\[\033[0;94m\]'
+    ["bright_purple"]='\[\033[0;95m\]'
+    ["bright_cyan"]='\[\033[0;96m\]'
+    ["bright_white"]='\[\033[0;97m\]')
 
 # background
-colour_background_black='\[\033[40m\]'
-colour_background_red='\[\033[41m\]'
-colour_background_green='\[\033[42m\]'
-colour_background_yellow='\[\033[43m\]'
-colour_background_blue='\[\033[44m\]'
-colour_background_purple='\[\033[45m\]'
-colour_background_cyan='\[\033[46m\]'
-colour_background_white='\[\033[47m\]'
-# bright
-colour_background_bright_black='\[\033[100m\]'
-colour_background_bright_red='\[\033[101m\]'
-colour_background_bright_green='\[\033[102m\]'
-colour_background_bright_yellow='\[\033[103m\]'
-colour_background_bright_blue='\[\033[104m\]'
-colour_background_bright_purple='\[\033[105m\]'
-colour_background_bright_cyan='\[\033[106m\]'
-colour_background_bright_white='\[\033[107m\]'
- 
+declare -A bg=(
+    ["black"]='\[\033[40m\]'
+    ["red"]='\[\033[41m\]'
+    ["green"]='\[\033[42m\]'
+    ["yellow"]='\[\033[43m\]'
+    ["blue"]='\[\033[44m\]'
+    ["purple"]='\[\033[45m\]'
+    ["cyan"]='\[\033[46m\]'
+    ["white"]='\[\033[47m\]'
+    ["bright_black"]='\[\033[100m\]'
+    ["bright_red"]='\[\033[101m\]'
+    ["bright_green"]='\[\033[102m\]'
+    ["bright_yellow"]='\[\033[103m\]'
+    ["bright_blue"]='\[\033[104m\]'
+    ["bright_purple"]='\[\033[105m\]'
+    ["bright_cyan"]='\[\033[106m\]'
+    ["bright_white"]='\[\033[107m\]')
 
 colour_reset='\[\033[0m\]'     # Text Reset]'
 
--- prompt/prompt.sh
+++ prompt/prompt.sh
@@ -1,22 +1,20 @@
 source ~/.prompt/colour.sh
 
-
-# $1= text, $2= foreground, $3= background, $4= bg colour as foreground, $5= next bg colour
+# $1= text, $2= foreground, $3= background, $4= next bg colour
 function prompt_section {
 	local prompt_section_div=' '
-	echo $2$3$1$4$5$prompt_section_div$colour_reset
+	echo "${fg[$2]}${bg[$3]}$1${fg[$3]}${bg[$4]}$prompt_section_div$colour_reset"
 }
 
 # Standard PS1: green \# grey \A white \u@\h:\W \$
 function prompt_std_ps1 {
-	local ps_command=$( prompt_section '\#' $colour_bright_yellow $colour_background_green $colour_green $colour_background_bright_black )
-	local ps_time=$( prompt_section '\A' $colour_white $colour_background_bright_black $colour_bright_black $colour_background_bright_white )
+	local ps_command=$( prompt_section '\#' bright_yellow green bright_black )
+	local ps_time=$( prompt_section '\A' white bright_black bright_white )
 	#Doesn't assign a next background to leave it as shell default.
-	local ps_location=$( prompt_section '\u@\h:\W \$' $colour_black $colour_background_bright_white $colour_bright_white '' )
+	local ps_location=$( prompt_section '\u@\h:\W \$' black bright_white black )
 
-	prompt_ps1=$ps_command$ps_time$ps_location
+    echo -n $ps_command$ps_time$ps_location
 }
 
 # TODO temp until we have git.
-prompt_std_ps1
-PS1=$prompt_ps1
+PS1=`prompt_std_ps1`
