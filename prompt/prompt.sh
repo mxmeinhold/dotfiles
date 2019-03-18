bdde13df84dfc07c9455472f3ddba5ca71d39d07
Max Meinhold <mxmeinhold@gmail.com>
2019-03-18 08:13:53 -0400

    Correct check for ssh agent
    Don't spit an error message if it isn't running.

--- prompt/prompt.sh
+++ prompt/prompt.sh
@@ -21,6 +21,6 @@ function prompt_std_ps1 {
 # TODO temp until we have git.
 PS1=`prompt_std_ps1`
 
-if [ "`ssh-add -l`" ]; then
+if [ $(ps -u $(whoami) | grep ssh-agent | wc -l) -gt 0 ]; then
     PS1="$(prompt_section '!!!' bright_white bright_red green)$PS1"
 fi
