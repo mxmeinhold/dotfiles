16fea63d7139cd20c5e1a5c3fba72432f2869fa9
Max Meinhold <mxmeinhold@gmail.com>
2019-03-18 08:54:07 -0400

    Add caching vim plugins

--- vimplugins.sh
+++ vimplugins.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 INFO="\e[1m\e[93m"
+ERR="\e[1m\e[91m"
 RESET="\e[0m"
 
 function info {
     echo "${INFO}---> $1$RESET"
+}
+
+function err {
+    echo "${ERR}---> $1$RESET"
 }
 
 
@@ -16,36 +21,40 @@ PLUGINS["auto-pairs"]="https://github.com/jiangmiao/auto-pairs"
 # Status bar
 PLUGINS["lightline"]="https://github.com/itchyny/lightline.vim"
 
-#TODO for each plugin add it as a remote, and pull it in, then move everything to .vim
+if ping -c 1 github.com &>/dev/null; then
+    echo -e $(info "Gathering vim plugins")
+else
+    echo -e $(err "Cannot connect to github, skipping.")
+    exit
+fi
+
 
 DOTFILES=`pwd`
 DIR="${DOTFILES}/symlink/.vim"
+GIT="${DOTFILES}/tmp/vim-git"
 
-mkdir -p ${DIR}/plugin
-mkdir -p ${DIR}/git
+mkdir -p ${DIR}
+mkdir -p ${GIT}
 
-cd ${DIR}/git
+cd ${GIT}
 git init
+for remote in $(git remote); do
+    if [ !${PLUGINS[$remote]} ]; then
+        git remote remove $remote
+    else
+        git remote update $remote
+        git merge --allow-unrelated-histories -s recursive -X ours --no-edit --no-gpg-sign -q $remote/master
+    fi
+done
 for plugin in ${!PLUGINS[*]}; do
     git remote add $plugin ${PLUGINS[$plugin]}
     git fetch --depth=1 -n -q $plugin
-    MERGES="$MERGES $plugin/master"
-    INIT="$plugin/master"
-done
-echo -e $(info "Merging $INIT")
-git reset --hard $INIT
-for branch in $MERGES; do
-    if [ "$INIT" != $branch ]; then
-        echo -e $(info "Merging $branch")
-        git merge --allow-unrelated-histories -s recursive -X ours --no-edit --no-gpg-sign -q $branch
-    fi
+        git merge --allow-unrelated-histories -s recursive -X ours --no-edit --no-gpg-sign -q $plugin/master
 done
 
-rm -rf .git
 for dir in *; do
     if [ -d $dir ]; then
         cp -r $dir $DIR/
     fi
 done
 cd ${DOTFILES}
-rm -rf ${DIR}/git
